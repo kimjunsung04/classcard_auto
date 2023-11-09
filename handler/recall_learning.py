c3feb73e0260f50b2cf967325d389d884180b9e5
kimjunsung04 <phservice.kr@gmail.com>
2023-11-09 20:52:13 +0900

    🐛 리콜학습 인식문제 수정

--- handler/recall_learning.py
+++ handler/recall_learning.py
@@ -11,7 +11,7 @@ class RecallLearning:
 
     def run(self, num_d: int, word_d: list) -> None:  # 핸들러 실행
         driver = self.driver
-        da_e, _, da_kyn = word_d
+        da_e, da_k, _ = word_d
         driver.find_element(
             By.XPATH,
             "/html/body/div[2]/div/div[2]/div[1]/div[2]",
@@ -21,7 +21,7 @@ class RecallLearning:
             By.CSS_SELECTOR,
             "#wrapper-learn > div.start-opt-body > div > div > div > div.m-t > a",
         ).click()  # 리콜학습 시작 버튼
-        time.sleep(1.5)
+        time.sleep(2)
         for i in range(num_d):  # 단어 수 만큼 반복
             try:
                 cash_d = driver.find_element(
@@ -29,40 +29,31 @@ class RecallLearning:
                     f"//*[@id='wrapper-learn']/div[1]/div/div[2]/div[2]/div[{i+1}]/div[1]/div/div/div/div[1]/span",
                 ).text  # 메인 단어 추출
 
-                cash_dby = [  # 선택창에서 모름 제외 3개 단어 추출
-                    driver.find_element(
-                        By.XPATH,
-                        f"//*[@id='wrapper-learn']/div[1]/div/div[2]/div[2]/div[{i+1}]/div[3]/div[{j+1}]/div[2]",
-                    ).text
-                    for j in range(3)
-                ]
-
-                ck = False  # 찾을수없는 단어 감지
-                if cash_d.upper() != cash_d.lower():  # 단어가 영어일때
-                    with contextlib.suppress(Exception):
-                        for j in range(3):  # 3개의 선택지중
-                            # 단어가 같은것이 있으면
-                            if da_e.index(cash_d) == da_kyn.index(cash_dby[j]):
-                                driver.find_element(  # 클릭
-                                    By.XPATH,
-                                    f"//*[@id='wrapper-learn']/div/div/div[2]/div[2]/div[{i+1}]/div[3]/div[{j+1}]",
-                                ).click()
-                                ck = True  # 찾음
-                                break
-                    if ck != True:  # 못찾았으면
-                        print("\n찾을수없는 단어 감지로 랜덤으로 찍기발동!!\n")  # 랜덤으로 찍기
-                        driver.find_element(  # 랜덤으로 클릭
-                            By.XPATH,
-                            f"//*[@id='wrapper-learn']/div/div/div[2]/div[2]/div[{i+1}]/div[3]/div[{random.randint(1, 3)}]",
-                        ).click()
-                        time.sleep(2)
-                        with contextlib.suppress(Exception):  # 예외 발생시 (긴급탈출)
-                            driver.find_element(
-                                By.XPATH,
-                                "//*[@id='wrapper-learn']/div/div/div[3]/div[2]",
-                            ).click()
-                time.sleep(2)  # 2초 대기
-            except Exception:  # 예외 발생시 (긴급탈출)
+                choice_list_element = driver.find_element(
+                    By.XPATH,
+                    f"//*[@id='wrapper-learn']/div[1]/div/div[2]/div[2]/div[{i+1}]/div[3]",
+                )
+                for choice_item in choice_list_element.find_elements(
+                    By.TAG_NAME, "div"
+                ):
+                    choice_text = choice_item.text
+                    if choice_text in da_k:
+                        if cash_d == da_e[da_k.index(choice_text)]:
+                            choice_item.click()
+                            break
+                    elif choice_text in da_e:
+                        if cash_d == da_k[da_e.index(choice_text)]:
+                            choice_item.click()
+                            break
+                    else:
+                        continue
+                else:
+                    print("모르는 단어 감지됨")
+                    choice_list_element.find_elements(By.TAG_NAME, "div")[
+                        random.randint(0, 3)
+                    ].click()
+                time.sleep(3)
+            except Exception as e:  # 예외 발생 시 학습 종료
                 driver.find_element(
                     By.XPATH, "/html/body/div[1]/div/div[1]/div[1]"
                 ).click()  # 학습 종료 버튼 클릭
