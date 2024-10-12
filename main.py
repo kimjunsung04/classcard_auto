9e8dd34053eb93426d0832531baaeeb101262515
EthanWonCho <realethanwoncho@gmail.com>
2024-10-12 15:35:43 +0900

    코드 가독성 향상

--- main.py
+++ main.py
@@ -33,28 +33,30 @@ chrome_options.add_argument('--log-level=1')
 # 드라이버 생성
 driver = webdriver.Chrome(options=chrome_options)
 
+# 로그인 시행
 driver.get("https://www.classcard.net/Login")
-tag_id = driver.find_element(By.ID, "login_id")
-tag_pw = driver.find_element(By.ID, "login_pwd")
-tag_id.clear()
-tag_id.send_keys(account["id"])
-tag_pw.send_keys(account["pw"])
+id_element = driver.find_element(By.ID, "login_id")
+pw_element = driver.find_element(By.ID, "login_pwd")
+id_element.clear() # Autofill 억제
+id_element.send_keys(account["id"])
+pw_element.send_keys(account["pw"])
 time.sleep(1)
 driver.find_element(
     By.XPATH,
     "/html/body/div[1]/div/div/div/div/form/div[3]/a",
-).click()
+).click()  # 로그인 버튼
+time.sleep(1)  # 로딩 대기
 
-time.sleep(1)  # 로그인이 늦어지는 경우를 대비
-
+# 클래스 선택
 class_dict = {}
 class_list_element = driver.find_element(
     By.CSS_SELECTOR,
     "body > div.mw-1080 > div:nth-child(6) > div > div > div.left-menu > div.left-item-group.p-t-none.p-r-lg > div.m-t-sm.left-class-list",
 )
+class_count = len(class_list_element.find_elements(By.TAG_NAME, "a"))
 for class_item, i in zip(
     class_list_element.find_elements(By.TAG_NAME, "a"),
-    range(len(class_list_element.find_elements(By.TAG_NAME, "a"))),
+    range(class_count),
 ):
     class_temp = {}
     class_temp["class_name"] = class_item.text
@@ -69,24 +71,24 @@ if len(class_dict) == 0:
 elif len(class_dict) == 1:
     choice_class_val = 0
 else:
-    choice_class_val = choice_class(class_dict=class_dict)  # 클래스 선택
+    choice_class_val = choice_class(class_dict=class_dict)  # 클래스 입력
+
 class_id = class_dict[choice_class_val].get("class_id")  # 클래스 아이디 가져오기
-
 driver.get(f"https://www.classcard.net/ClassMain/{class_id}")  # 클래스 페이지로 이동
-
 time.sleep(1)  # 로딩 대기
 
-sets_list = []
+# 세트 선택
 sets_div = driver.find_element(
     By.XPATH, "/html/body/div[1]/div[2]/div/div/div[2]/div[3]/div"
 )
 sets = sets_div.find_elements(By.CLASS_NAME, "set-items")
+sets_count = len(sets)
 sets_dict = {}
-for set_item, i in zip(sets, range(len(sets))):
+for set_item, i in zip(sets, range(sets_count)):
     set_temp = {}
-    set_temp["card_num"] = (  # 카드 개수 가져오기(10 카드)
+    set_temp["card_num"] = (
         set_item.find_element(By.TAG_NAME, "a").find_element(By.TAG_NAME, "span").text
-    )
+    )  # 카드 개수 가져오기 예) "10 카드"
     set_temp["title"] = set_item.find_element(By.TAG_NAME, "a").text.replace(
         set_temp["card_num"], ""
     )  # 카드 개수 제거
@@ -94,19 +96,15 @@ for set_item, i in zip(sets, range(len(sets))):
         "data-idx"
     )  # 세트 아이디 가져오기
     sets_dict[i] = set_temp
-choice_set_val = choice_set(sets_dict)  # 세트 선택
 
-set_site = (
-    f"https://www.classcard.net/set/{sets_dict[choice_set_val]['set_id']}/{class_id}"
-)
-
+set_choice = choice_set(sets_dict)  # 세트 입력
+set_site = (f"https://www.classcard.net/set/{sets_dict[set_choice]['set_id']}/{class_id}")
 driver.get(set_site)  # 세트 페이지로 이동
-time.sleep(1)
+time.sleep(1)  # 로딩 대기
 
-user_id = int(driver.execute_script("return c_u;"))  # 유저 아이디 가져오기
+user_id = int(driver.execute_script("return c_u;"))  # API 요청을 위해 유저 아이디 가져오기
 
-ch_d = chd_wh()  # 학습유형 선택
-
+# 단어 저장
 driver.find_element(
     By.CSS_SELECTOR,
     "body > div.test > div.p-b-sm > div.set-body.m-t-25.m-b-lg > div.m-b-md.pos-relative > div.dropdown > a",
@@ -115,16 +113,14 @@ driver.find_element(
     By.CSS_SELECTOR,
     "body > div.test > div.p-b-sm > div.set-body.m-t-25.m-b-lg > div.m-b-md.pos-relative > div.dropdown.open > ul > li:nth-child(1) > a",
 ).click()  # 학습구간 전체로 변경
-
 html = BeautifulSoup(driver.page_source, "html.parser")  # 페이지 소스를 html로 파싱
 cards_ele = html.find("div", class_="flip-body")  # 카드들을 찾음
 num_d = len(cards_ele.find_all("div", class_="flip-card")) + 1  # 카드의 개수를 구함
-
 time.sleep(0.5)  # 로딩 대기
-
 word_d = word_get(driver, num_d)  # 단어를 가져옴
-
 da_e, da_k, da_kyn = word_d
+
+ch_d = chd_wh()  # 학습유형 입력
 while 1:
     if ch_d == 1:
         print("암기학습은 지원하지 않습니다.")
@@ -143,7 +139,7 @@ while 1:
         print("암기학습 API 요청을 시작합니다.")
         classcard_api_post(
             user_id=user_id,
-            set_id=sets_dict[choice_set_val]["set_id"],
+            set_id=sets_dict[set_choice]["set_id"],
             class_id=class_id,
             view_cnt=num_d,
             activity=1,
@@ -152,7 +148,7 @@ while 1:
         print("리콜학습 API 요청을 시작합니다.")
         classcard_api_post(
             user_id=user_id,
-            set_id=sets_dict[choice_set_val]["set_id"],
+            set_id=sets_dict[set_choice]["set_id"],
             class_id=class_id,
             view_cnt=num_d,
             activity=2,
@@ -161,13 +157,11 @@ while 1:
         print("스펠학습 API 요청을 시작합니다.")
         classcard_api_post(
             user_id=user_id,
-            set_id=sets_dict[choice_set_val]["set_id"],
+            set_id=sets_dict[set_choice]["set_id"],
             class_id=class_id,
             view_cnt=num_d,
             activity=3,
         )
     print("학습이 종료되었습니다.")
-    driver.get(set_site)  # 다시 세트페에지로 이동
-    ch_d = chd_wh()  # 다시 선택
-    driver.get(set_site)  # 다시 세트페에지로 이동
+    driver.get(set_site)  # 다시 세트페이지로 이동
     time.sleep(1)
